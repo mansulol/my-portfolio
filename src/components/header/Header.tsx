1bd72bf2b09fc7e5d5b69adaa2d8721219cdd755
Mansour <mansourlol440@gmail.com>
2025-06-26 01:06:54 +0300

    Base Page

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import './Header.scss'
+
+export default function Header() {
+    return (
+        <header id='header' >
+            <h1>Mansour</h1>
+            <ul>
+                <li>
+                    <a href="#"> Projects </a>
+                </li>
+                <li>
+                    <a href="#"> About Me </a>
+                </li>
+            </ul>
+        </header>
+    )
+}
---
+++ src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,10 @@
+import "./LandingPage.scss"
+
+export default function LandingPage() {
+    return (
+        <section >
+            <img src="images\avatar.png" alt="" />
+            <h1 id="name" > Mansour </h1>
+        </section>
+    )
+}
