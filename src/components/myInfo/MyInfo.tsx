6ec75bf966183b5b60cbf52eef00aab25c23e1d4
Mansour <mansourlol440@gmail.com>
2025-07-14 15:45:03 +0100

    Section my info

--- src/components/myInfo/MyInfo.tsx
+++ src/components/myInfo/MyInfo.tsx
@@ -1,14 +1,18 @@
+// Components
+import { Link } from "react-router-dom"
+
+// Custom styles
 import "./MyInfo.scss"
 
 export default function MyInfo() {
    return (
       <section id="section-my-info">
          <div className="container-my-name">
-            <h4 className="text-my-name-my-info">
+            <h2 className="text-my-name-my-info">
                Hi, I'm <span className="my-name">Mansour</span>
                <br />
                I love to create and design websites and applications.
-            </h4>
+            </h2>
          </div>
          <div className="container-more-about-me">
             <article className="more-about-me-1">
@@ -17,13 +21,13 @@ export default function MyInfo() {
             <article className="more-about-me-2">
                Hi, my name is Mansour, and I am a web developer with a passion for creating beautiful and functional websites. I have experience in various web technologies and enjoy working on projects that challenge my skills and creativity.
             </article>
-            <div className="more-about-me-container">
-               <h4 className="text-more-about-me-title">
+            <Link className="more-about-me-container" to={"/about"} >
+               <h3 className="text-more-about-me-title">
                   About Me
-               </h4>
+               </h3>
                <img src="https://cdn-icons-png.flaticon.com/512/6941/6941884.png " alt="Img top left arrow" />
 
-            </div>
+            </Link>
          </div>
       </section>
    )
