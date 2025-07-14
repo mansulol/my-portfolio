a0caccc9bcc59e4ea1dabc7ccc3e03dccaf4605b
Mansour <mansourlol440@gmail.com>
2025-07-14 09:49:46 +0100

    Hero section in home

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,15 @@
+import "./Hero.scss"
+
+export default function Hero() {
+    return (
+        <section id="section-hero" >
+            <div className="section-hero-wrapper">
+                <h2 id="text-section-hero" >
+                    Lorem <span>ipsum</span> odor <br />
+                    amet, consectetuer <br />
+                    adipiscing
+                </h2>
+            </div>
+        </section>
+    )
+}
