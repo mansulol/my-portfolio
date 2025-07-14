127579e051b957c862a0b3e599f69e4f44a4a98a
Mansour <mansourlol440@gmail.com>
2025-07-14 19:51:36 +0100

    Footer in progress

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.scss"
+
+export default function Footer() {
+   return (
+      <footer id="footer">
+         <div className="footer-content">
+            <p>&copy; 2023 Mansour. All rights reserved.</p>
+         </div>
+      </footer>
+   )
+}
