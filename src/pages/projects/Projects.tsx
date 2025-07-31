a97c51fb7f31ca2c84e1dd2a2bdd2b4b2705d634
Mansour Lo Lo <mansourlol440@gmail.com>
2025-07-31 18:42:01 +0100

    project updated with hero and works section

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,9 +1,29 @@
-import './Projects.scss'
+import Hero from "@components/hero/Hero";
+import "./Projects.scss";
+import Header from "@components/header/Header";
+import Footer from "@components/footer/Footer";
 
 export default function Projects() {
     return (
-        <div>
-            <p> Projects page </p>
+        <div id="page-projects">
+            <Header />
+            <Hero>
+                <section className="container-my-works">
+                    <div className="container-title-works">
+                        <h2 className="text-section-hero"> My <span> Works </span> </h2>
+                    </div>
+                    <div className="container-bento-projects">
+                        <div className="bento-grid-projects-wrapper">
+                            <div className="bento-grid grid-1"></div>
+                            <div className="bento-grid grid-2"></div>
+                            <div className="bento-grid grid-3"></div>
+                            <div className="bento-grid grid-4"></div>
+                            <div className="bento-grid grid-5"></div>
+                        </div>
+                    </div>
+                </section>
+            </Hero>
+            <Footer />
         </div>
-    )
+    );
 }
