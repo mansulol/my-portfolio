031fe8fcccd3c71f9202a96c51f9cba4911f3687
Mansour Lo Lo <mansourlol440@gmail.com>
2025-08-05 20:50:45 +0100

    About page in progress

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,10 +1,33 @@
+import Hero from '@components/hero/Hero'
+import Header from '@components/header/Header'
+
+// Styles
 import './About.scss'
+import Footer from '@components/footer/Footer'
+import SongCard from '@components/cards/songCard/SongCard'
 
 export default function About() {
     return (
-        <div>
+        <div id='page-about'>
+            <Header />
+            <Hero>
+                <div className="container-about-me">
+                    <div className="song-cards-wrapper">
+                        <SongCard />
+                        <SongCard />
+                        <SongCard />
+                        <SongCard />
+                    </div>
+                    <div className="gallery-photos-wrapper">
 
-            <p> About Page </p>
+                    </div>
+                    <div className="about-me-info-wrapper">
+
+                    </div>
+                </div>
+            </Hero>
+
+            <Footer />
 
         </div>
     )
