38d1d5b8cbfcc151e0ac0a149a3710dea0e9e967
Mansour <mansour@192.168.1.135>
2025-08-12 19:02:45 +0100

    song card components

--- src/components/cards/songCard/SongCard.tsx
+++ src/components/cards/songCard/SongCard.tsx
@@ -1,13 +1,32 @@
+// Types
+import type { SongProps } from '@/types/commons';
+
+// Components
+import AudioPlayer from 'react-h5-audio-player';
+import GlassEffectCard from '@components/cards/glassEffectCard/GlassEffectCard';
 
 // Styles
 import './songCard.scss'
+import 'react-h5-audio-player/lib/styles.css';
 
-export default function SongCard() {
+export default function SongCard({ song }: { song: SongProps }) {
+
     return (
         <div className="song-card">
-            <h3 className="song-title">Song Title</h3>
-            <p className="song-artist">Artist Name</p>
-            <p className="song-description">This is a brief description of the song.</p>
+            <img className='song-card-image' src={song.coverImage} alt={song.title} />
+            <div className="container-song-card-media-controller">
+                <GlassEffectCard>
+                    <AudioPlayer
+                        src={song.audioFileURL}
+                        showFilledProgress={false}
+                        showSkipControls={false}
+                        showJumpControls={true}
+                        customAdditionalControls={[]}
+                        customProgressBarSection={[]}
+                        customVolumeControls={[]}
+                    />
+                </GlassEffectCard>
+            </div>
         </div>
     );
 }
