d6e31d6d8eff3e87e07833a646cdd743872852c2
Mansour Lo Lo <mansourlol440@gmail.com>
2025-08-19 19:41:12 +0100

    contact me section now for about page

---
+++ src/components/contactMe/ContactMe.tsx
@@ -0,0 +1,26 @@
+// Components
+import ContactMeCard from "@components/cards/contactMeCard/ContactMeCard";
+
+// Images
+import paperPlaneImage from '@/assets/images/paper-plane.svg';
+
+// Styles
+import './ContactMe.scss';
+
+export default function ContactMe() {
+    return (
+        <div id="section-contact-me">
+            <div className="container-svg-contact-me">
+                <img src={paperPlaneImage} alt="Paper Plane" className="svg-contact-me" />
+            </div>
+            <div className="wrapper-title-contact-me">
+                <h3 className="text-title-contact-me"> Contact Me </h3>
+            </div>
+            <div className="container-cards-contact-me">
+                <ContactMeCard title="Resume" urlLink="https://mansulol.github.io/my-portfolio/docs/CV_Programing_en.pdf"/>
+                <ContactMeCard title="Photos" />
+                <ContactMeCard title="Archives" />
+            </div>
+        </div>
+    );
+}
