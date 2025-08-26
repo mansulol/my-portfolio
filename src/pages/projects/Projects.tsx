ff4aa700af434ec5f4ad17ae1e8f15c8d7d20a41
Mansour Lo Lo <mansourlol440@gmail.com>
2025-08-26 09:42:34 +0100

    Bento grids with project cards

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -3,6 +3,7 @@ import Hero from "@components/hero/Hero";
 import Header from "@components/header/Header";
 import Footer from "@components/footer/Footer";
 import projectWorks from "@services/projects.service";
+import ProjectCard from '@/components/cards/projectCard/ProjectCard';
 
 // Types
 // import type { projectCard } from "@/types/project.type"
@@ -15,40 +16,61 @@ export default function Projects() {
         <div id="page-projects">
             <Header />
             <Hero>
-
-                {
-                    // Toca prepar los bentos para recibir datos
-                    // De paso puedo crear un componente tsx BentoGrid privado para guardar la info
-                    // Y recorrer los proyectos e impimir esa funcion
-                }
-
-
                 <section className="container-my-works">
                     <div className="container-title-works">
                         <h2 className="text-section-hero"> My <span> Works </span> </h2>
                     </div>
                     <div className="container-bento-projects">
                         <div className="bento-grid-projects-wrapper">
-                            <div className="bento-grid grid-1">
-                               { projectWorks[0].title }
+                            {/* {projectWorks.map((project, i) => (
+                                <div key={i} className={`bento-grid grid-${i + 1}`}>
+                                    <ProjectCard
+                                        pos={0}
+                                        projectCard={project}
+                                    />
+                                </div>
+                            ))} */}
+                            <div className={`bento-grid grid-1`}>
+                                <ProjectCard
+                                    pos={0}
+                                    projectCard={projectWorks[0]}
+                                />
                             </div>
-                            <div className="bento-grid grid-2">
-                                {projectWorks[1].title}
+                            <div className={`bento-grid grid-2`}>
+                                <ProjectCard
+                                    pos={1}
+                                    projectCard={projectWorks[1]}
+                                />
                             </div>
-                            <div className="bento-grid grid-3">
-                                {projectWorks[2].title}
+                            <div className={`bento-grid grid-3`}>
+                                <ProjectCard
+                                    pos={1}
+                                    projectCard={projectWorks[2]}
+                                />
                             </div>
-                            <div className="bento-grid grid-4">
-                                {projectWorks[3].title}
+                            <div className={`bento-grid grid-4`}>
+                                <ProjectCard
+                                    pos={0}
+                                    projectCard={projectWorks[3]}
+                                />
                             </div>
-                            <div className="bento-grid grid-5">
-                                {projectWorks[4].title}
+                            <div className={`bento-grid grid-5`}>
+                                <ProjectCard
+                                    pos={0}
+                                    projectCard={projectWorks[4]}
+                                />
                             </div>
-                            <div className="bento-grid grid-6">
-                                {projectWorks[5].title}
+                            <div className={`bento-grid grid-6`}>
+                                <ProjectCard
+                                    pos={1}
+                                    projectCard={projectWorks[5]}
+                                />
                             </div>
-                            <div className="bento-grid grid-7">
-                                {projectWorks[6].title}
+                            <div className={`bento-grid grid-7`}>
+                                <ProjectCard
+                                    pos={1}
+                                    projectCard={projectWorks[6]}
+                                />
                             </div>
                         </div>
                     </div>
